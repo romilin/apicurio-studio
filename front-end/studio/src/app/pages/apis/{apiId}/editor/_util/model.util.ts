bbdf83b222674abc0147017c5d940400d3c26b7b
Romain MILIN <romain.milin@partnre.com>
2023-03-16 11:45:49 +0100

    Exemple allOf

--- front-end/studio/src/app/pages/apis/{apiId}/editor/_util/model.util.ts
+++ front-end/studio/src/app/pages/apis/{apiId}/editor/_util/model.util.ts
@@ -116,7 +116,16 @@ export class ModelUtils {
      */
     public static generateExampleFromSchema(schema: OasSchema | AaiSchema): any {
         let generator: ExampleGenerator = new ExampleGenerator();
-        return generator.generate(schema);
+        let example : any[] = [];
+        example.push(generator.generate(schema));
+        if (schema.allOf) {
+            schema.allOf.forEach( inherited => {
+                if (inherited.$ref) {
+                    example.push(generator.generate(inherited));
+                }
+            });
+        }
+        return example;
     }
 
 }
@@ -163,6 +172,9 @@ export class ExampleGenerator {
         }
     }
 
+    /**
+     * Generates an exemple if no example exist
+    **/
     private generateObject(schema: OasSchema | AaiSchema): any {
         let object: any = {};
         if (schema.properties) {
